879cf591a4b2831d2ef66628840706027e83eef2
netmeta <moron686@gmail.com>
2023-02-04 03:06:47 +0300

    проект

--- Infrastructure.cs
+++ Infrastructure.cs
@@ -1,16 +1,18 @@
 using static System.Console;
 using static System.String;
 
-public static class Infrastructure
+public static class Infrastructure // создает и печатает
 {
-    public static int[] CreateArray(int size, int min = 0, int max = 10)
+    public static int[] CreateArray(this int size, int min = 0, int max = 10)
     {
         return Enumerable.Range(1, size)
                 .Select(item => Random.Shared.Next(min, max))
                 .ToArray();
     }
-    public static void Print(int[] array)
+    public static int[] Print(this int[] array, string separator = ",")
     {
-        WriteLine($"[{Join(',', array)}]");
+        string output =Join(separator,array);
+        WriteLine($"[{output}]");
+        return array;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using static Sorting;
 using static Infrastructure;
-int[] array = CreateArray(10);
-Print(array);
-SelectionSort(array);
-Print(array);
+// int[] array = CreateArray(10, 100,200);
+// Print(array);
+// SelectionSort(array);
+// Print(array);
 
+10.CreateArray()
+    .Print(",")
+    .SelectionSort()
+    .Print("|");
